7058db927b1e8a0db48f639c65f1b1e9ee04b250
Thong Phan <phtrithong@gmail.com>
2022-12-18 05:51:34 +0700

    [feat] Add WhatsNew section

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,8 +1,45 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { TypingText, NewFeatures, TitleText } from "../components";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { newFeatures } from "../constants";
 
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className={`${styles.paddings} relative z-10`} id="new">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="| What's New?" />
+        <TitleText title={<>What's new about Metaversus</>} />
+        <div className="mt-[48px] flex lg:flex-row flex-col gap-[24px] justify-between">
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={planetVariants("right")}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/whats-new.png"
+          alt="whats-new"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
   </section>
 );
 
