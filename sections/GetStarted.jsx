3eca4c22278440561ca4824e86e37d2b808e04bf
Thong Phan <phtrithong@gmail.com>
2022-12-18 01:16:30 +0700

    [feat] draft

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,22 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText, StartSteps, TitleText } from "../components";
+import styles from "../styles";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { startingFeatures } from "../constants";
 
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col`}
+    >
+      <div />
+    </motion.div>
   </section>
 );
 
