97a4ff7df6344844a50f8391a243aabb97cd6b09
Thong Phan <phtrithong@gmail.com>
2022-12-18 05:44:44 +0700

    [bug] fix: can't scroll to top after navigate to #explore

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Footer, Navbar } from "../components";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
 
 const Page = () => (
-  <div className="bg-primary-black overflow-hidden">
+  <div className="bg-primary-black">
     <Navbar />
     <Hero />
     <div className="relative">
