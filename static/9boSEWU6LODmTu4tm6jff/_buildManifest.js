9e052a39a2a3ef8d8274b691f8db69ef6fc358e2
Thong Phan <phtrithong@gmail.com>
2022-12-20 21:26:36 +0700

    Updates

---
+++ static/9boSEWU6LODmTu4tm6jff/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/910-2c43cb349485e376.js","static/chunks/pages/index-23a978b500d6ebfc.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
