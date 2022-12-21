8710c08bb9716123ef1e6447a42d3422ddd25b1f
Thong Phan <phtrithong@gmail.com>
2022-12-22 01:12:07 +0700

    [bug] try using next's Image

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { motion } from "framer-motion";
 import styles from "../styles";
 import { slideIn, staggerContainer, textVariant } from "../utils/motion";
@@ -32,14 +33,18 @@ const Hero = () => (
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
         <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
-        <img
+        <Image
+          width={500}
+          height={350}
           src="/cover.png"
           alt="cover"
           className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
         />
         <a href="#explore">
           <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
-            <img
+            <Image
+              width={155}
+              height={155}
               src="/stamp.png"
               alt="stamp"
               className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
